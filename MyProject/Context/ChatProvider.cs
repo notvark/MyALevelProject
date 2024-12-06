5b878595cac76026570dcf4eea645b5a0eb09a4e
X <qbrext@gmail.com>
2024-12-06 14:46:03 +0000

    Done with messaging, continue further implementation

--- MyProject/Context/ChatProvider.cs
+++ MyProject/Context/ChatProvider.cs
@@ -9,14 +9,25 @@ namespace MyProject.Context
     {
         private readonly DatabaseContext _context;
 
+        public ChatProvider(DatabaseContext context)
+        {
+            _context = context;
+        }
+
         public async Task<List<Chat>> RetrieveTextsOrdered(User currentUser, User chatToUser)
         {
             return await _context.Chats
-                .Where(chat => chat.FromUserId == currentUser.Id && chat.ToUserId == chatToUser.Id)
+                .Where(chat =>
+                    (chat.FromUserId == currentUser.Id && chat.ToUserId == chatToUser.Id) || //Or 
+                    (chat.FromUserId == chatToUser.Id && chat.ToUserId == currentUser.Id)) //Switching receiving to client and client to receiving
                 .OrderBy(chat => chat.SentDateTime)
                 .ToListAsync();
         }
 
+        public async Task SendMessageAsync(Chat chat)
+        {
+            _context.Chats.Add(chat);
+            await _context.SaveChangesAsync();
+        }
     }
-
 }
